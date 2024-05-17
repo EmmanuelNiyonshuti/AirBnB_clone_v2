d0df33f89d7e34f5be6e6887d31e5cb2dc7e2d2c
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-17 07:18:13 +0200

    fixing check fail

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,21 +2,15 @@
 #sets up  web servers for the deployment of web_static
 
 #Install Nginx if it not already installed
-apt-get install -y nginx
+sudo apt-get install -y nginx
 #create necessary directories
-sudo mkdir -p /data/web_static
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir  /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-#create a file with some contents in test directory
 sudo touch /data/web_static/releases/test/index.html
-
-#recursivery Give ownership of the /data/ folder to the ubuntu user AND group
-# sudo chown -R ubuntu: /data/
-sudo chown -R ubuntu: /data/
-sudo chown -R ubuntu: /data/web_static
-
 echo "
 <html>
    <head>
@@ -26,11 +20,13 @@ echo "
    </body>
 </html>" > /data/web_static/releases/test/index.html
 
-#create a symbolic link if it doesn't already exists
 if [ -L /data/web_static/current ]; then
 	sudo rm /data/web_static/current
 fi
-sudo ln -s /data/web_static/releases/test  /data/web_static/current
+sudo ln -s /data/web_static/releases/test/  /data/web_static/current
+
+#recursivery Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu: /data/
 
 #add a location block for /hbnb_static in sites-available/default file
 sudo sed -i '/server_name _;/a\\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}' /etc/nginx/sites-available/default
@@ -41,5 +37,4 @@ sudo ln -s /etc/nginx/sites-available/default  /etc/nginx/sites-enabled/
 
 sudo nginx -t
 
-sudo service nginx restart
-
+sudo service nginx reload
