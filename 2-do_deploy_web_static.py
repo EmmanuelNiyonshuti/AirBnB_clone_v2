836a45ca796b75023e125d0e309e18b512198a8a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-17 14:04:00 +0200

    fixing code check fail2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,17 +20,17 @@ def do_deploy(archive_path):
     :param archive_path: The path to the .tgz archive to be deployed.
     """
     if os.path.isfile(archive_path):
+
         put(archive_path, '/tmp/')
-
         curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        new = "web_static_{}".format(curr_time)
+        new_dir = "web_static_{}".format(curr_time)
 
-        run('sudo mkdir -p /data/web_static/releases/{}/'.format(new))
-
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(os.path.basename(archive_path), new))
+        run('sudo mkdir -p /data/web_static/releases/{}/'.format(new_dir))
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(os.path.basename(archive_path), new_dir))
         run('sudo rm /tmp/{}'.format(os.path.basename(archive_path)))
-        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(new, new))
-        run('sudo rm -rf /data/web_static/releases/{}/web_static/'.format(new))
+        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(new_dir, new_dir))
+        run('sudo rm -rf /data/web_static/releases/{}/web_static/'.format(new_dir))
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(new))
-
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(new_dir))
+    else:
+        return False
