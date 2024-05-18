641e5fa29df51e287893ec062282c12930eee329
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-18 17:43:49 +0200

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,22 +20,25 @@ def do_deploy(archive_path):
 
     :param archive_path: The path to the .tgz archive to be deployed.
     """
-    try:
-        if os.path.isfile(archive_path):
-            put(archive_path, '/tmp/')
-            curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
-            new_dir = "web_static_{}".format(curr_time)
+    curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    new_dir = "web_static_{}".format(curr_time)
 
-            run('sudo mkdir -p /data/web_static/releases/{}/'.format(new_dir))
-            run(('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+    if not os.path.isfile(archive_path):
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p /data/web_static/releases/{}/'.format(new_dir))
+        run(('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
                 .format(os.path.basename(archive_path), new_dir)))
-            run('sudo rm /tmp/{}'.format(os.path.basename(archive_path)))
-            run(('sudo mv /data/web_static/releases/{}/web_static/* '
+        run('sudo rm /tmp/{}'.format(os.path.basename(archive_path)))
+        run(('sudo mv /data/web_static/releases/{}/web_static/* '
                 '/data/web_static/releases/{}/'.format(new_dir, new_dir)))
-            run('sudo rm -rf /data/web_static/releases/{}/web_static/'
-                .format(new_dir))
-            run('sudo rm -rf /data/web_static/current')
-            run(('sudo ln -s /data/web_static/releases/{}/ '
+        run('sudo rm -rf /data/web_static/releases/{}/web_static/'
+            .format(new_dir))
+        run('sudo rm -rf /data/web_static/current')
+        run(('sudo ln -s /data/web_static/releases/{}/ '
                 '/data/web_static/current').format(new_dir))
+        return True
     except Exception:
-        return None
+        return False
+
