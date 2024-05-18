2a3904043bcfbb06e7101118596abc61fd89689e
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-18 17:55:47 +0200

    2-do_deploy_web_static.py new line at the end

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,12 +26,12 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         run('sudo mkdir -p /data/web_static/releases/{}/'.format(new_dir))
         run(('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-                .format(os.path.basename(archive_path), new_dir)))
+             .format(os.path.basename(archive_path), new_dir)))
         run('sudo rm /tmp/{}'.format(os.path.basename(archive_path)))
         run(('sudo mv /data/web_static/releases/{}/web_static/* '
-                '/data/web_static/releases/{}/'.format(new_dir, new_dir)))
+             '/data/web_static/releases/{}/'.format(new_dir, new_dir)))
         run('sudo rm -rf /data/web_static/releases/{}/web_static/'
             .format(new_dir))
         run('sudo rm -rf /data/web_static/current')
         run(('sudo ln -s /data/web_static/releases/{}/ '
-                '/data/web_static/current').format(new_dir))
+             '/data/web_static/current').format(new_dir))
