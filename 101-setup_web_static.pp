d2b24379cca0b185a518ccdca6fd67fc1f518d9a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-05-19 18:11:30 +0200

    puppet for configuring nginx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,12 +7,12 @@ package { 'nginx':
   install_options => ['-y'],
 }
 
-# Ensure the directories are created
+# Ensure the directories are created with recursive ownership and permissions
 file { '/data/':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
   recurse => true,
 }
 
@@ -57,17 +57,14 @@ file { '/data/web_static/releases/test/index.html':
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
-#remove symbolic link if it already exists
-file {'/data/web_static/current':
-    ensure => 'absent',
-}
 
+# Ensure the symbolic link is present, removing it first if it exists
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
+  force  => true, # Removes existing symlink if it exists
   owner  => 'ubuntu',
   group  => 'ubuntu',
-  require => File['/data/web_static/releases/test'],
 }
 
 # Update Nginx configuration
@@ -75,6 +72,20 @@ file_line { 'nginx_hbnb_static':
   path  => '/etc/nginx/sites-available/default',
   line  => 'location /hbnb_static { alias /data/web_static/current; }',
   after => 'server_name _;',
+}
+
+# Ensure the symbolic link for Nginx sites-enabled/default
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'absent', # Remove existing file if it exists
+}
+
+file { '/etc/nginx/sites-enabled':
+  ensure => 'directory', # Ensure /etc/nginx/sites-enabled is a directory
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
 }
 
 # Ensure Nginx config test and reload
