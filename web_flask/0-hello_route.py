b21d03d87142918d1cb3af21bc2e2b7c550c8c89
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-05-31 11:20:09 +0200

    typo

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""starts a Flask web application listening on all Ips on the same network."""
+"""Starts a Flask web application listening on all IPs on the same network."""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello():
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
     """Displays Hello HBNB"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
