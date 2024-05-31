9c6dd1130968123c6e31709a5df7fd7702cb2205
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-05-31 11:29:36 +0200

    fix a requirement fail

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Starts a Flask web application listening on all IPs on the same network."""
-
+"""
+Starts a Flask web application
+listening on all IPs on the same network.
+"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays Hello HBNB"""
     return "Hello HBNB!"
 
-app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
