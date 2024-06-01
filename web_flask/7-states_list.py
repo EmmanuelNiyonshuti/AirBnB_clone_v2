8874e86bb90fe8c17494a0e1e2f78979e2fd48ab
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-01 22:31:06 +0200

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """list all states objects in a format <state.id>: <B><state.name></B>""""
+    """list all states objects in a format <state.id>: <B><state.name></B>"""
     states_objs = storage.all(State).values()
     return render_template('7-states_list.html', states=states_objs)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """close the connection to the database""""
+    """close the connection to the database"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
