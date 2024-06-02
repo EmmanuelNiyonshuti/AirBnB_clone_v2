97da2ded34ee6b0673173526ae95dd0a0e1994c6
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-02 11:13:47 +0200

    fixed a typo in 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,18 +3,10 @@
 Starts a flask web application for AirBnB clone.
 """
 import os
-# from os import getenv
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy import create_engine
-
-
-# db_url = "mysql+mysqldb://{}:{}@{}/{}".format(
-#         getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
-#         getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB'))
     
 app = Flask(__name__)
 
@@ -31,13 +23,10 @@ def states_cities():
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
 
         states_objs = storage.all(State).values()
-        # session = Session()
-        # states_objs = session.query(State).all()
-        # # session.close()
         return render_template('8-cities_by_states.html', states=states_objs)
     else:
         states_objs = storage.all(State).values()
         return render_template('8-cities_by_states.html', states=states_objs)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
