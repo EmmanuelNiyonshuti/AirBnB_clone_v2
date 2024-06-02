5c68e793885a6a46f6e77225c9127e84d6e2a402
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-02 10:56:09 +0200

    fixed a typo in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object"""
+"""This module instantiates storage object and reloads all objects from the storage engines"""
 import os
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
