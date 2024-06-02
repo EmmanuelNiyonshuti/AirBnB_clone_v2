a15338b0e8202d02ef2761d94c4cb3b5f0791417
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-02 12:31:27 +0200

    fixed a requirement error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,18 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """list all states objects in a format <state.id>: <B><state.name></B>"""
+    state_objs = storage.all(State)
+    return render_template('7-states_list.html', states=state_objs)
+
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """close the connection to the database"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """list all states objects in a format <state.id>: <B><state.name></B>"""
-    state_objs = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_objs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
