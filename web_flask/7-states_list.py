043b9f4ea7262975232584a2c61284d535a3967d
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-03 02:27:17 +0200

    sorted the states by their attribute name in task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Starts a flask web application for AirBnB clone.
+Starts a flask web application for AirBnB clone and
+comprises a '/states_list' route function for displaying the states objs.
 """
 from flask import Flask, render_template
 from models import storage
@@ -11,7 +12,9 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def display_states():
-    """list all states objects in a format <state.id>: <B><state.name></B>"""
+    """
+    list all states objects in a format <state.id>: <B><state.name></B>
+    """
     state_objs = storage.all(State)
     return render_template('7-states_list.html', states=state_objs)
 
